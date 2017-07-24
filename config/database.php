779ff72ef326fc9c9b334fde6a79c1ad97466b16
Samir-romdhani <romdhanisamir94@gmail.com>
2017-07-24 02:45:37 +0100

    Update database.php

--- config/database.php
+++ config/database.php
@@ -1,9 +1,9 @@
 <?php
 // used to connect to the database
-$host = "localhost";
-$db_name = "1phpbeginnercrudlevel1";
-$username = "root";
-$password = "";
+$host = "us-cdbr-iron-east-03.cleardb.net";
+$db_name = "heroku_24b9f95d4c7ac81";
+$username = "bd38260874d9e2";
+$password = "3a2208f3";
  
 try {
     $con = new PDO("mysql:host={$host};dbname={$db_name}", $username, $password);
